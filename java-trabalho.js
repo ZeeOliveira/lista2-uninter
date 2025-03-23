ed9444c539b70cc7f0952c67bc7fbbb51c440579
ZeeOliveira <zippihlive@gmail.com>
2025-03-23 17:24:49 -0300

    Add files via upload

---
+++ java-trabalho.js
@@ -0,0 +1,13 @@
+let corfundo1 = "blue";
+let corfundo2 = "black";
+
+
+function MudarCor() {
+    if (document.body.style.backgroundColor=== corfundo1)
+    
+        {document.body.style.backgroundColor= corfundo2;} 
+     else {
+            document.body.style.backgroundColor= corfundo1;
+     } 
+}
+
